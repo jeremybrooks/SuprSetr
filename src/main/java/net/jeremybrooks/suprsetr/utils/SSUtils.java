fbcb7461fda8353b1ab8c223ae726d80251cb756
jeremybrooks <jeremyb@whirljack.net>
2014-06-23 21:08:08 -0700

    Update copyright

--- src/main/java/net/jeremybrooks/suprsetr/utils/SSUtils.java
+++ src/main/java/net/jeremybrooks/suprsetr/utils/SSUtils.java
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with SuprSetr.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package net.jeremybrooks.suprsetr.utils;
 
 import net.jeremybrooks.jinx.response.photos.Photo;
